65b97293aa204b3114b2553af1a9bbeda522805d
unknown <kblman@gmail.com>
2020-08-05 21:24:51 +0200

    Leccion 9: Logs en Flask

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
-from flask_login import  LoginManager
+import logging
+from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 
 
@@ -16,6 +17,9 @@ def create_app(settings_module):
         app.config.from_pyfile('config-testing.py', silent=True)
     else:
         app.config.from_pyfile('config.py', silent=True)
+
+    # Configurar loggin de la app
+    configure_logging(app)
 
     login_manager.init_app(app)
     login_manager.login_view = "login"
@@ -45,3 +49,63 @@ def register_error_handlers(app):
     @app.errorhandler(404)
     def error_404_handler(e):
         return render_template('404.html'), 404    
+
+
+def configure_logging(app):
+
+    # Elimina los manejadores por defecto de la app
+    del app.logger.handlers[:]
+
+    loggers = [app.logger, )]  # logging.getLogger('sqlalchemy')]  logger de la libreria SQLAlchemy
+    handlers = []
+
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(verbose_formatter())
+    
+    if (app.config['APP_ENV'] == app.config['APP_ENV_LOCAL']) or (
+            app.config['APP_ENV'] == app.config['APP_ENV_TESTING']) or (
+            app.config['APP_ENV'] == app.config['APP_ENV_DEVELOPMENT']):
+        console_handler.setLevel(logging.DEBUG)
+        handlers.append(console_handler)
+    elif app.config['APP_ENV'] == app.config['APP_ENV_PRODUCTION']:
+        console_handler.setLevel(logging.INFO)
+        handlers.append(console_handler)
+        mail_handler = SMTPHandler((app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+                                   app.config['DONT_REPLY_FROM_EMAIL'],
+                                   app.config['ADMINS'],
+                                   '[Error][{}] La aplicación falló'.format(app.config['APP_ENV']),
+                                   (app.config['MAIL_USERNAME'],
+                                    app.config['MAIL_PASSWORD']),
+                                   ())
+        mail_handler.setLevel(logging.ERROR)
+        mail_handler.setFormatter(mail_handler_formatter())
+        handlers.append(mail_handler)
+
+    for l in loggers:
+        for handler in handlers:
+            l.addHandler(handler)
+        l.propagate = False
+        l.setLevel(logging.DEBUG)
+
+
+def verbose_formatter():
+    return logging.Formatter(
+        '[%(asctime)s.%(msecs)d]\t %(levelname)s \t[%(name)s.%(funcName)s:%(lineno)d]\t %(message)s',
+        datefmt='%d/%m/%Y %H:%M:%S'
+    )        
+
+def mail_handler_formatter():
+    return logging.Formatter(
+        '''
+            Message type:       %(levelname)s
+            Location:           %(pathname)s:%(lineno)d
+            Module:             %(module)s
+            Function:           %(funcName)s
+            Time:               %(asctime)s.%(msecs)d
+
+            Message:
+
+            %(message)s
+        ''',
+        datefmt='%d/%m/%Y %H:%M:%S'
+    )
--- app/public/routes.py
+++ app/public/routes.py
@@ -1,18 +1,25 @@
-from flask import abort, render_template
+import logging
+from flask import abort, render_template, current_app
 from werkzeug.exceptions import NotFound
 
 from . import public_bp
 from app.models import Post
 
+logger = logging.getLogger(__name__)
 
 @public_bp.route("/")
 def index():
+    logger.info('Mostrando los posts del blog')
+
     posts = Post.get_all()
     return render_template("public/index.html", posts=posts)
 
 
 @public_bp.route("/p/<string:slug>/")
 def show_post(slug):
+    logger.info('Mostrando un post')
+    logger.debug(f'Slug: {slug}')
+    
     post = Post.get_by_slug(slug)
     if post is None:
         raise NotFound(slug)
