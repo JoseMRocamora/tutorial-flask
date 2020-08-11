2ee360ce422aa705e208b74a2689387c13ad194f
unknown <kblman@gmail.com>
2020-08-11 17:52:10 +0200

    Preparar app para subir a heroku

--- app/__init__.py
+++ app/__init__.py
@@ -86,6 +86,7 @@ def configure_logging(app):
     elif app.config['APP_ENV'] == app.config['APP_ENV_PRODUCTION']:
         console_handler.setLevel(logging.INFO)
         handlers.append(console_handler)
+"""
         mail_handler = SMTPHandler((app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
                                    app.config['DONT_REPLY_FROM_EMAIL'],
                                    app.config['ADMINS'],
@@ -96,6 +97,7 @@ def configure_logging(app):
         mail_handler.setLevel(logging.ERROR)
         mail_handler.setFormatter(mail_handler_formatter())
         handlers.append(mail_handler)
+"""
 
     for l in loggers:
         for handler in handlers:
